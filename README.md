8c2b4c5506b2db3ff7873683cd6e55d6ed550b50
Sy Lim <slimdoe2@gmail.com>
2019-03-27 15:04:19 -0700

    almost done

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # bamazon
 bamazon 10HW
+
+<!-- Complex data
+ arrays ["value", "value1"]
+ objects { key: "value", key1: "value" }
+
+how to access them
+    arrays 
+        name_of_array[1] or 
+        var i=1;
+        name_of_array[i]
+
+    objects JON
+        name_of_objects--JON.key // dot notation
+        name_of_objects--JON ["key"] // square notation
+        var key="key"
+        name_of_objects--JON[key] // square notation
+
+
+how to add [1, 2, 3, "jon"]
+    name_of_array[name_of_array.length] = "asdfasdf"
+    cat[cat.length]=jon
+    
+how to update (jon)
+    name_of_array[1] = "asdfasdf"
+    cat[1]=jon -->
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 var mysql = require('mysql');
 
-var connect = mysql.createConnection({
+var inquirer = require ('inquirer');
+
+var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
@@ -8,8 +10,89 @@ var connect = mysql.createConnection({
     database: 'bamazon_DB'
 });
 
-connect.connect(function (err) {
-    if (err) throw err;
-    console.log('connected as id' + connect.threadId);
-    connect.end();
+connection.connect(function (err) {
+    if (err) return console.log(err);
+    console.log('connected as id' + connection.threadId);
+
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) return console.log(err);
+        // Log all results of the SELECT statement
+        console.log(res);
+
+        /*
+        var i;
+        for (i = 0; i < cars.length; i++) { 
+            text += cars[i] + "<br>";
+        }
+        */
+        var i;
+        for (i = 0; i < res.length; i++) {
+            //console.log (res[i])
+            console.log(res[i].product_name + " " + res[i].department_name + " " + res[i].price)
+            // console.log(res[i].department_name)
+            // console.log(res[i].price)
+
+        }
+        function start() {
+            connection.query("SELECT * FROM products", function(err, res) {
+                if (err) throw err;
+            inquirer
+              .prompt([
+                  {
+                    type: "input",
+                    message: "\nEnter the ID of the item you want to purchase.",
+                    name: "itemId"
+                  },
+                  {
+                    type: "input",
+                    message: "How many units do you want to purchase?\n",
+                    name: "amount"
+                  }
+         
+              ])
+              .then(function(answer) {
+                for (var i = 0; i < res.length; i++) {
+         
+                    chosenItem = res[i].product_name;
+                    if (res[i].item_id == answer.itemId && res[i].stock_quantity>=answer.amount) {
+                      //chosenAmount= answer.amount;
+                      console.log("You want to buy " + parseFloat(answer.amount)+" "+ chosenItem);
+                      start();
+         
+                    }else if(res[i].item_id == answer.itemId && res[i].stock_quantity<<answer.amount){
+                        console.log("Sorry we don't have enough "+chosenItem+". There are only "+res[i].stock_quantity+" available.");
+                        start();
+                   }
+         
+                  }
+              });
+          });
+         };
+
+        
+       
+
+
+        connection.end();
+    });
+
+
 });
+
+
+
+  
+// 5. Then create a Node application called `bamazonCustomer.js`. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+
+// 6. The app should then prompt users with two messages.
+
+//    * The first should ask them the ID of the product they would like to buy.
+//    * The second message should ask how many units of the product they would like to buy.
+
+// 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+//    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+
+// 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+//    * This means updating the SQL database to reflect the remaining quantity.
+//    * Once the update goes through, show the customer the total cost of their purchase.
